63ca6b65d7f83d1b5a8213a5f893997799ed07e0
Your Name <you@example.com>
2019-11-17 22:33:43 +0400

    first

---
+++ home.php
@@ -0,0 +1,36 @@
+<?php
+ include("head.html");
+ // include("header.html");
+ include("navbar.html");
+?>
+
+
+<body>
+	
+	<?php
+		echo "fuck";
+		echo "<p></P>";
+		echo "fuck";
+		echo "<p></P>";
+		echo "fuck";
+		echo "<p></P>";
+		echo "fuck";
+		echo "<p></P>";
+		echo "fuck";
+		echo "<p></P>";
+		echo "fuck";
+		echo "<p></P>";
+		echo "fuck";
+		echo "<p></P>";
+		echo "fuck";
+		echo "<p></P>";
+		echo "fuck";
+		echo "<p></P>";
+		echo "fuck";
+		echo "<p></P>";
+	?>
+</body>
+
+<?php
+ include("footer.html");
+?>
